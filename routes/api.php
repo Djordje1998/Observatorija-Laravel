31816a829526bb6d56c50fe5372728da8fc2dc2c
Djordje1999 <djordjen7@gmail.com>
2022-01-23 00:17:48 +0100

    Dodate rute preko resursa

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\ObservationController;
+use App\Http\Controllers\ScientistController;
+use App\Http\Controllers\StarController;
+use App\Http\Resources\StarResource;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +21,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+//Route::get('stars',[StarController::class,'index']);
+//Route::get('stars/{id}',[StarController::class,'show']);
+
+Route::resource('stars',StarController::class);
+Route::resource('scientists',ScientistController::class);
+Route::resource('observations',ObservationController::class);
