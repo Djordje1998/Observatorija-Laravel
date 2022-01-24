2fa20dd50a9e9090836663742b7423b742a8e2c1
Djordje1999 <djordjen7@gmail.com>
2022-01-24 23:52:59 +0100

    Implementirane rute za registraciju i login

---
+++ app/Http/Controllers/API/AuthController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(),[
+            'name'=>'required|string|max:50|min:3',
+            'email'=>'required|string|max:50|min:8|email|unique:users',
+            'password'=>'required|string|max:50|min:5'
+        ]);
+
+        if($validator->fails()){
+            return response()->json($validator->errors());
+        }
+        $user = User::create([
+            'name'=>$request->name,
+            'email'=>$request->email,
+            'password'=>Hash::make($request->password)
+        ]);
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+        return response()->json(['data'=>$user,'access_token'=>$token,'token_type'=>'Bearer']);
+
+    }
+
+    public function login(Request $request)
+    {
+        if(!Auth::attempt($request->only('email','password'))){
+            return response()->json(['message'=>'Unauthorized'],401);
+        }
+        $user = User::where('email',$request['email'])->firstOrFail();
+        $token = $user->createToken('auth_token')->plainTextToken;
+        return response()->json(['message'=>'Hi '.$user->name.' welcome to Observation.',
+            'acess_token'=>$token,'token_type'=>'Bearer']);
+    }
+}
