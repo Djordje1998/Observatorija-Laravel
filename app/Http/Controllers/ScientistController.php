11e67fceb29c97a3a63d8e64a81ee1b5b9c6645c
Djordje1999 <djordjen7@gmail.com>
2022-01-31 13:14:30 +0100

    Promenjana poruka za api u slucaju greske

--- app/Http/Controllers/ScientistController.php
+++ app/Http/Controllers/ScientistController.php
@@ -41,27 +41,26 @@ class ScientistController extends Controller
      */
     public function store(Request $request)
     {
-        $validator = Validator::make($request->all(),[
-            'name'=>'required|string|max:50|min:3',
-            'email'=>'required|string|max:50|min:8|email|unique:scientists',
-            'password'=>'required|string|max:50|min:5'
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:50|min:3',
+            'email' => 'required|string|max:50|min:8|email|unique:scientists',
+            'password' => 'required|string|max:50|min:5'
         ]);
 
-        if($validator->fails()){
-            return response()->json(['success' => false,'message'=>"".$validator->errors()]);
+        if ($validator->fails()) {
+            return response()->json(['success' => false, 'message' => "" . $validator->errors()]);
         }
 
         try {
             $scientist = Scientist::create([
-                'name'=>$request->name,
-                'email'=>$request->email,
-                'password'=>Hash::make($request->password)
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => Hash::make($request->password)
             ]);
-            return response()->json(['success' => true,'message'=>'Scientist is created successfully.','scientist'=>new ScientistResource($scientist)]);
+            return response()->json(['success' => true, 'message' => 'Scientist is created successfully.', 'scientist' => new ScientistResource($scientist)]);
         } catch (Exception $ex) {
             return response()->json(['success' => false, 'message' => $ex->getMessage()]);
         }
-
     }
 
     /**
@@ -95,27 +94,27 @@ class ScientistController extends Controller
      */
     public function update(Request $request, $scientist_id)
     {
-        $validator = Validator::make($request->all(),[
-            'name'=>'required|string|max:50|min:3',
-            'email'=>'required|string|max:50|min:8|email|unique:scientists',
-            'password'=>'required|string|max:50|min:5'
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:50|min:3',
+            'email' => 'required|string|max:50|min:8|email|unique:scientists',
+            'password' => 'required|string|max:50|min:5'
         ]);
 
-        if($validator->fails()){
+        if ($validator->fails()) {
             return response()->json($validator->errors());
         }
 
         $scientist = Scientist::find($scientist_id);
-        if(is_null($scientist)){
+        if (is_null($scientist)) {
             return response()->json('Scientist with given id does not exist!', 404);
         }
 
         $scientist->update([
-            'name'=>$request->name,
-            'email'=>$request->email,
-            'password'=>Hash::make($request->password)
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
         ]);
-        return response()->json(['Scientist is created successfully.',new ScientistResource($scientist)]);
+        return response()->json(['Scientist is created successfully.', new ScientistResource($scientist)]);
     }
 
     /**
@@ -128,9 +127,14 @@ class ScientistController extends Controller
     {
         $scientist = Scientist::find($scientist_id);
         if (is_null($scientist)) {
-            return response()->json(['success'=>false,'message'=>'Scientist with given id does not exist!'], 404);
+            return response()->json(['success' => false, 'message' => 'Scientist with given id does not exist!'], 404);
         }
-        $scientist->delete();
-        return response()->json(['success'=>true,'message'=>'Scientist successfully deleted'],200);
+        
+        try {
+            $scientist->delete();
+            return response()->json(['success' => true, 'message' => 'Scientist successfully deleted'], 200);
+        } catch (Exception $ex) {
+            return response()->json(['success' => false, 'message' => $ex->getMessage()]);
+        }
     }
 }
